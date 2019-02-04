c74680a1cbff057a487f43e3ed682e00b410817f
yamunaver94 <yamunaver94@gmail.com>
2019-02-04 21:57:20 +0530

    Coding style complies with PEP 8

--- guessing_game.py
+++ guessing_game.py
@@ -4,48 +4,56 @@ Project 1 - Number Guessing Game
 --------------------------------
 """
 
+
+# Coding style complies with PEP 8.
 import random
 import sys
-highscore=[]
+highscore = []
+
+
 def start_game():
-	print ("------------------------------")
-	print("Welcome to Number Guessing Game!")
-	print ("------------------------------")
-	tries=0
-	random_no = random.randint(1,10)
-	while  True :
-		take = True
-		while take:
-			try:
-				guess = input("Pick a Number between 1 and 10 : ")
-				guess = int (guess)
-			except ValueError:
-				print("Please enter an integer")
-			else:
-				take = False
-		if guess<1 or guess>=11 :
-			print("Please enter number between 1 and 10(including) !!!!!!!\n TRY AGAIN!!!!")
-		else:
-			tries+=1
-			if guess > random_no :
-				print ("It is lower!")
-			elif guess < random_no :
-				print("It is higher!")
-			elif guess==random_no :
-				print ("You got it! It took you {} tries.".format(tries))
-				ans = input("Would you like play again ? [y]es /[n]o: ")
-				ans = ans.lower()
-				if ans=="n" :
-					print("Closing the game see you next time . BYE!!!!")
-					sys.exit()
-				elif ans=="y":
-					highscore.append(tries)
-					highscore.sort()
-					print("Get a score under {} to beat the highscore!".format(highscore[0]))
-					tries=0
-					random_no = random.randint(1,10)
-					continue
-				else :
-					print ("Please enter 'y' or 'n'.")
+    print("------------------------------")
+    print("Welcome to Number Guessing Game!")
+    print("------------------------------")
+    tries = 0
+    random_no = random.randint(1, 10)
+    while True:
+        take = True
+        while take:
+            try:
+                guess = input("Pick a Number between 1 and 10 : ")
+                guess = int(guess)
+            except ValueError:
+                print("Please enter an integer")
+            else:
+                take = False
+        if guess < 1 or guess >= 11:
+            print("""Please enter number between 1 and 10(including) !!!!!!!\n
+            TRY AGAIN!!!!""")
+        else:
+            tries += 1
+            if guess > random_no:
+                print("It is lower!")
+            elif guess < random_no:
+                print("It is higher!")
+            elif guess == random_no:
+                print("You got it! It took you {} tries.".format(tries))
+                ans = input("Would you like play again ? [y]es /[n]o: ")
+                ans = ans.lower()
+                if ans == "n":
+                    print("Closing the game see you next time . BYE!!!!")
+                    sys.exit()
+                elif ans == "y":
+                    highscore.append(tries)
+                    highscore.sort()
+                    print("Get a score under {} to beat the highscore!"
+                          .format(highscore[0]))
+                    tries = 0
+                    random_no = random.randint(1, 10)
+                    continue
+                else:
+                    print("Please enter 'y' or 'n'.")
+
+
 if __name__ == '__main__':
-	start_game()
+    start_game()
